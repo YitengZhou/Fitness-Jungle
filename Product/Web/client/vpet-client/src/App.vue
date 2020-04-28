848b9e5eee0cc1936772ca79e7ccbdcf9f9d67c2
lihw <MajyoArisu@outlook.com>
2020-04-28 09:21:31 +0100

    add Menu

--- Product/Web/client/vpet-client/src/App.vue
+++ Product/Web/client/vpet-client/src/App.vue
@@ -1,17 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<el-container class="bg-white">
+  <el-header>
+    <el-menu :default-active="activeIndex" router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="/">Profile</el-menu-item>
+      <el-menu-item index="/document">Pet</el-menu-item>
+      <el-menu-item index="/createJob">Friends</el-menu-item>
+      <el-menu-item index="/jobList">Map</el-menu-item>
+    </el-menu>
+  </el-header>
+  <el-main>
+    <router-view/>
+  </el-main>
+  <el-footer></el-footer>
+</el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
