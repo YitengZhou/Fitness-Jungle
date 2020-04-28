62ed22b3f4763823e5461c233e4832e2aa46a44a
Lewis N <wee_ng@hotmail.com>
2020-04-28 22:10:05 +0800

    added pet controller and new api /pet/getAllTypes

--- Product/Web/server/routes.js
+++ Product/Web/server/routes.js
@@ -1,8 +1,11 @@
 const apiURI = "/api";
 const userControllerURI = "/user";
+const petControllerURI = "/pet";
 const userController = require("./controllers/user");
+const petController = require("./controllers/pet");
 
 module.exports = function (app, express, path, rootDir) {
    app.get(apiURI + userControllerURI + "/getAllUsers", userController.getAllUsers);
+   app.get(apiURI + petControllerURI + "/getAllTypes", petController.getAllTypes);
    app.use(express.static(rootDir));
 };
