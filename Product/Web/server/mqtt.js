3fd38628e879be85f72f890a7845d78586653979
Lewis N <wee_ng@hotmail.com>
2020-05-18 13:57:11 +0800

    added mqtt connectivity

--- Product/Web/server/mqtt.js
+++ Product/Web/server/mqtt.js
@@ -1,5 +1,8 @@
 const mqtt = require('mqtt');
 const channel = 'fitnessjungle/1';
+const mqttName = 'server';
+const desktopMqttName = 'desktop';
+const m5MqttName = 'm5';
 let client = mqtt.connect('mqtt://broker.mqttdashboard.com');;
 
 client.on('connect', function () {
@@ -7,12 +10,22 @@ client.on('connect', function () {
    client.subscribe(channel);
 });
 
-// client.publish('messages', 'Current time is: ' + new Date());
-
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log(message.toString());
-})
+  try {
+     let reqObj = JSON.parse(message.toString());
+     console.log(reqObj);
+     if (reqObj["to"] == mqttName) {
+        let resObj = {"from": mqttName, "to": m5MqttName, "message": "message received"};
+        client.publish(channel, JSON.stringify(resObj));
+     }     
+
+  } 
+  catch (err) {
+     console.log(err);
+  }
+  
+});
 
 module.exports = {
    client
