ab21e9d146ed12778a1d1bdc9a7b5eead4331fda
LewisGitting <wee_ng@hotmail.com>
2020-05-22 06:50:49 +0800

    Web feature update (#34)
    * Create the vpet-client project
    
    * add Menu
    
    * add Pet Page
    
    * add Profile Page and Login Page
    
    * added desktop mqtt api /getUserList
    
    * added desktop mqtt api /getUserDetailed
    
    * added desktop mqtt api getUserStepsInterval
    
    * data management
    
    * add ajax
    
    * chart modify
    
    * update communication
    
    Co-authored-by: lihw <MajyoArisu@outlook.com>
    Co-authored-by: YitengZhou <56918168+YitengZhou@users.noreply.github.com>

---
+++ Product/Web/client/vpet-client/src/components/chart/stepCart.vue
@@ -0,0 +1,30 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  data: () => ({
+    chartdata: {
+      labels: ['1st', '5th', '10th', '15th', '20th', '25th'],
+      datasets: [
+        {
+          label: 'Steps',
+          backgroundColor: '#87ceeb',
+          data: [25, 20, 17, 29, 22, 21]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style>
+</style>
